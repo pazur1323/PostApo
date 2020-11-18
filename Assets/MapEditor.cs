7021979101ea320e828bf1b55d76abde76903fcf
Michal Gorczyca <mich.gorc@gmail.com>
2020-11-18 21:10:51 +0100

    Initial commit

---
+++ Assets/MapEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MapEditor : MonoBehaviour
+{
+    public int gridSize = 10;
+
+    private void Update() {
+        Vector3 pos = GetPosition();
+        transform.position = new Vector3(pos.x*gridSize, pos.y*gridSize, pos.z*gridSize);
+        WritePositionInName();
+    }
+    
+    Vector3 GetPosition()
+    {
+        return new Vector3(
+                Mathf.RoundToInt(transform.position.x/gridSize),
+                Mathf.RoundToInt(transform.position.y/gridSize),
+                Mathf.RoundToInt(transform.position.z/gridSize)
+                );
+    }
+
+    void WritePositionInName(){
+
+        int x = (int)GetPosition().x;
+        int y = (int)GetPosition().y;
+        int z = (int)GetPosition().z;
+        gameObject.name = "(" + x + ", " + y + ", " + z + ")";
+    }
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0f, 1f, 0f);
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move(){
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool isHit = Physics.Raycast(ray, out hit);
+        if(isHit){
+
+            if(Input.GetMouseButtonDown(0)){
+                navMeshAgent.destination = hit.point;
+            }
+        }
+    }
+}
